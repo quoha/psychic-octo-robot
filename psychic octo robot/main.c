828526e11fb2487a6050c5c3e2386def7ad5a8df
Michael Henderson <quoha@quoha.org>
2013-01-25 17:43:34 -0700

    add the basic types - string, stack and bucket

--- psychic octo robot/main.c
+++ psychic octo robot/main.c
@@ -7,7 +7,46 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+//--------------------------------------------------------------
+//
+typedef struct PORBUK PORBUK;
+typedef struct PORSTK PORSTK;
+typedef struct PORSTR PORSTR;
+struct PORBUK {
+    int   (*c)(PORBUK *root, PORSTK *stack, PORSTR *string);
+    PORBUK *s[27];
+};
+struct PORSTR {
+    char *curr;
+    char  data[1];
+};
+struct PORSTK {
+    void *p;
+};
+
+//--------------------------------------------------------------
+//
+int POR_Execute(PORBUK *root, PORSTK *stack) {
+    return 0;
+}
+
+//--------------------------------------------------------------
+//
+PORSTR *POR_String(const char *string) {
+    string = string ? string : "";
+    PORSTR *p = malloc(sizeof(*p) + strlen(string));
+    if (p) {
+        p->curr = p->data;
+        strcpy(p->data, string);
+    }
+    return p;
+}
+
+//--------------------------------------------------------------
+//
 int main(int argc, const char * argv[])
 {
 
