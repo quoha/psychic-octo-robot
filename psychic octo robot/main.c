c87d1b0d6a7200d0dd3cf2eb2ba4421cf796bd6e
Michael Henderson <quoha@quoha.org>
2013-01-25 20:48:41 -0700

    add eval() function

--- psychic octo robot/main.c
+++ psychic octo robot/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 //--------------------------------------------------------------
 //
@@ -21,11 +22,13 @@ struct PORBUK {
 };
 struct PORSTR {
     char *curr;
+    char *eod;
     char  data[1];
 };
 struct PORSTK {
     void *p;
 };
+PORSTR *POR_String(const char *string, int length);
 
 //--------------------------------------------------------------
 //
@@ -39,8 +42,32 @@ PORBUK *POR_Bucket(void) {
 
 //--------------------------------------------------------------
 //
-int POR_Execute(PORBUK *root, PORSTK *stack) {
-    return 0;
+int POR_Eval(PORBUK *root, PORSTK *stack, PORSTR *prog) {
+    PORBUK *b = root;
+    prog->curr = prog->data;
+    char *startOfWord = prog->curr;
+    while (prog->curr < prog->eod) {
+        if (b->s[*(prog->curr)]) {
+            b = b->s[*(prog->curr++)];
+        } else if (isspace(*(prog->curr)) && b->c) {
+            // execute
+            prog->curr++;
+            int rv = b->c(root, stack, prog);
+            if (!rv) {
+                return rv;
+            }
+            startOfWord = prog->curr;
+        } else {
+            // invalid token
+            PORSTR *word = POR_String(startOfWord, (int)(prog->curr + 1 - startOfWord));
+            if (word) {
+                printf("error:\tinvalid word '%s'\n", word->data);
+                free(word);
+            }
+            return 0;
+        }
+    }
+    return 1;
 }
 
 //--------------------------------------------------------------
@@ -55,9 +82,10 @@ PORSTR *POR_String(const char *string, int length) {
             memset(p, 0, sizeof(*p) + length);
         } else {
             memcpy(p->data, string, length);
-            p->data[length] = 0;
         }
         p->curr = p->data;
+        p->eod  = p->data + length;
+        *(p->eod) = 0;
     }
     return p;
 }
@@ -66,9 +94,12 @@ PORSTR *POR_String(const char *string, int length) {
 //
 int main(int argc, const char * argv[])
 {
+    PORBUK *root  = POR_Bucket();
+    PORSTR *prog  = POR_String("Hello, World\n", 12);
+    PORSTK *stack = 0;
 
-    // insert code here...
-    printf("Hello, World!\n");
+    POR_Eval(root, stack, prog);
+
     return 0;
 }
 
