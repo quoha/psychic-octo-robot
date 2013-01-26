07d4da71cb89de6dc331bc4084418ab7b1c61daa
Michael Henderson <quoha@quoha.org>
2013-01-25 17:32:03 -0700

    Initial Commit

---
+++ psychic octo robot/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  psychic octo robot
+//
+//  Created by Michael Henderson on 1/25/13.
+//  Copyright (c) 2013 Michael Henderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
